a19edbe0cc867263fbc5e28c27eb46b2c7b4cbfd
JayantGupta77 <gjayant517@gmail.com>
2025-12-13 20:28:13 +0530

    py

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use("/", userRouter);
 
 
 /*app.all("*", (req, res, next)=>{
-    next(new ExpressError(404, "Page not found!"));
+ next(new ExpressError(404, "Page not found!"));
 });*/
 
 
