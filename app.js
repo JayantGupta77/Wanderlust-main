3753dac8fde6f62082adad3ded1365362c6273cd
JayantGupta77 <gjayant517@gmail.com>
2025-12-24 21:48:03 +0530

    py

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
