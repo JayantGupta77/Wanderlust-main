9546a2f64989802dda6226b5d8a377d3db0c5a01
JayantGupta77 <gjayant517@gmail.com>
2025-12-27 22:36:57 +0530

    py

--- app.js
+++ app.js
@@ -101,7 +101,6 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 
-
 app.use((err, req, res, next)=>{
     let {statusCode = 500, message = "Something went wrong"} = err;
     res.status(statusCode).render("error.ejs", {message});
